c9c9124ffbedfc5ef23d2394f2b73f95f1203147
Prathamesh Ghute <prathameshghute6@gmail.com>
2026-01-04 19:10:20 +0530

    Episode 2

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
-const heading = React.createElement("h1",{id:"heading",xyz:"xyz"},"Hello world from React!");// {} indiacate the attribute to the tag like class,id etc all which we gives to html tag, 3rd parameter is the content inside the tag and props 
-console.log(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement("h1",{id:"heading",xyz:"xyz"},"Hello world from React!");// {} indiacate the attribute to the tag like class,id etc all which we gives to html tag, 3rd parameter is the content inside the tag and props 
+// console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
@@ -13,12 +16,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
  * 
  */
 
-const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"Hello from nested h1"))); // it is react element which is javascript object  and it is created using createElement method of react
+// const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"Hello from nested h1"))); // it is react element which is javascript object  and it is created using createElement method of react
 // ReactElement(object)  => HTML(Browser Understand) 
 // it convert the object into html
 
 
-console.log(parent);
+// console.log(parent);
 // root.render(parent);
 
 
@@ -32,7 +35,7 @@ console.log(parent);
  * 
  */
 
-const parent1 = React.createElement("div",{id:"parent1"},React.createElement("div",{id:"child1"},[React.createElement("h1",{},"Hello from nested h1"),React.createElement("h2",{},"Hello from nested h2")]));
+// const parent1 = React.createElement("div",{id:"parent1"},React.createElement("div",{id:"child1"},[React.createElement("h1",{},"Hello from nested h1"),React.createElement("h2",{},"Hello from nested h2")]));
 
 // root.render(parent1);
 
@@ -51,7 +54,7 @@ const parent1 = React.createElement("div",{id:"parent1"},React.createElement("di
  * 
  */
 
-const parent2 = React.createElement("div",{id:"parent2"},[React.createElement("div",{id:"child1"},[React.createElement("h1",{},"I'm h1 tag from child 1"),React.createElement("h2",{},"I'm h2 tag from child 1")]),React.createElement("div",{id:"child 2"},[React.createElement("h1",{},"I'm h1 tag from child 2"),React.createElement("h2",{},"I'm h2 tag from child 2")])]);
+const parent2 = React.createElement("div",{id:"parent2"},[React.createElement("div",{id:"child1"},[React.createElement("h1",{},"This is Namaste React "),React.createElement("h2",{},"I'm h2 tag from child 1")]),React.createElement("div",{id:"child 2"},[React.createElement("h1",{},"I'm h1 tag from child 2"),React.createElement("h2",{},"I'm h2 tag from child 2")])]);
 
 root.render(parent2);
 
---
+++ README.md
@@ -0,0 +1,22 @@
+# Namaste react
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement   //it means it use Auto - reloading
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification 
+- Bundling
+- Compress
+- Consistent Hashing - read 
+- Code Splitting
+- Differential Bundling - to support older browser
+- Diagnostic - proper error
+- Error Handling
+- HTTPS feature
+- Tree Shaking - remove unused code 
+- Different dev and production bundles
+
+
